395a4dcfb5d18df802d0f5307c0396dd4a513bb5
Flaki <git@flaki.hu>
2024-03-04 13:59:36 +0200

    Implement new ImageData()

--- src/js-canvas/ImageData.ts
+++ src/js-canvas/ImageData.ts
@@ -20,12 +20,45 @@ export class ImageData {
 	 */
 	readonly width: number;
 
-	constructor(sw: number, sh: number, settings?: ImageDataSettings);
-	constructor(data: Uint8ClampedArray, sw: number, sh?: number, settings?: ImageDataSettings);
-	constructor(swOrData,shOrSw, settingsOrSh?, settings?) {
-		console.log(`[HTMLCanvasElement] Not implemented: new ImageData( ${Array.from(arguments).join(', ')} )`);
-		this.width = 512;
-		this.height = 512;
-		this.data = new Uint8ClampedArray(this.width * this.height * 4);
+	constructor(width: number, height: number, settings?: ImageDataSettings);
+	constructor(data: Uint8ClampedArray, width: number, height?: number, settings?: ImageDataSettings);
+	constructor(widthOrData, heightOrWidth, settingsOrHeight?, settings?) {
+		const rgbaArray = (widthOrData instanceof Uint8ClampedArray) ? widthOrData : null;
+		let colorSpace = "srgb";
+
+		if (rgbaArray) {
+			console.log(`[ImageData] Warning: Array initializer support is experimental! ( new ImageData(${Array.from(arguments).join(',')}) )`);
+
+			this.width = heightOrWidth;
+			this.height = typeof settingsOrHeight === "number" ? settingsOrHeight : undefined;
+
+			colorSpace = ( typeof settingsOrHeight === "object" ? settingsOrHeight : ( typeof settings === "object" ? settings : { colorSpace: "srgb" } )).colorSpace;
+		} else {
+			this.width = widthOrData;
+			this.height = heightOrWidth;
+
+			colorSpace = ( typeof settingsOrHeight === "object" ? settingsOrHeight : { colorSpace: "srgb" }).colorSpace;
+		}
+
+		if (colorSpace !== "srgb") {
+			console.log(`[ImageData] Warning: Only the 'srgb' color space is supported! ( new ImageData(${Array.from(arguments).join(',')}) )`);
+			throw new Error(`Unsupported colorSpace: ${colorSpace}`);
+		}
+
+		// We need to calculate the height based on the array data and width
+		if (this.height === undefined) {
+			this.height = rgbaArray.length / this.width / 4;
+		}
+
+		if (rgbaArray) {
+			if (this.width * this.height * 4 !== rgbaArray.length) {
+				// TODO: this should be a "DOMException"
+				throw new Error(`The array dimensions do not match the supplied width or height`);
+			}
+
+			this.data = new Uint8ClampedArray(rgbaArray);
+		} else {
+			this.data = new Uint8ClampedArray(this.width * this.height * 4);
+		}
 	}
 };
